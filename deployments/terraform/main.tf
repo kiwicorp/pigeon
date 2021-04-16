aebbb7048eb66379e9bb3793503f5f8c6763a8a9
cezarmathe <me@cezarmathe.com>
2021-04-16 22:50:00 +0300

    Improve deployments
    Added tags, dynaomdb table for content, better naming.

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -16,13 +16,15 @@ module "gh_content_fn" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "1.45.0"
 
-  function_name = "lambda-function-test"
+  function_name = "content_github_releases"
   description   = "A test lambda function."
-  handler       = "cmd/lambda/lambda-linux-amd64"
+  handler       = "cmd/content_github_releases/content_github_releases-linux-amd64"
   runtime       = "go1.x"
 
   create_package         = false
-  local_existing_package = "../../cmd/lambda/lambda_dev_linux_amd64.zip"
+  local_existing_package = "../../cmd/content_github_releases/content_github_releases.zip"
+
+  tags = local.tags
 }
 
 module "gh_content_events" {
@@ -31,12 +33,34 @@ module "gh_content_events" {
 
   create_bus = false
 
-  rules = local.event_rules
-
+  rules   = local.event_rules
   targets = local.event_targets
+
+  tags = local.tags
+}
+
+module "content_table" {
+  source = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = "content-table"
+  hash_key = "urn"
+
+  attributes = [
+    {
+      name = "urn" # urn:pigeon.selftech.io:content:github_releases:/<repo_owner>/<repo_name>
+      type = "S"
+    }
+  ]
+
+  tags = local.tags
 }
 
 locals {
+  tags = {
+    PigeonVersion     = "dev"
+    PigeonReleaseName = "gh-content-demo-mvp"
+  }
+
   event_rule_names = [for gh_content in var.gh_content : "event_${gh_content.repo_owner}_${gh_content.repo_name}"]
 
   event_rules = { for index, gh_content in var.gh_content : local.event_rule_names[index] => {
@@ -44,13 +68,13 @@ locals {
     schedule_expression = "cron(0 0 ? * * *)"
   } }
 
-  event_targets = { for index, gh_content in var.gh_content : local.event_rule_names[index] => [ {
-    name     = "Execute lambda for ${gh_content.repo_owner}/${gh_content.repo_name}."
-    arn      = module.gh_content_fn.this_lambda_function_arn
+  event_targets = { for index, gh_content in var.gh_content : local.event_rule_names[index] => [{
+    name = "Execute lambda for ${gh_content.repo_owner}/${gh_content.repo_name}."
+    arn  = module.gh_content_fn.this_lambda_function_arn
     constant = jsonencode({
       access_token = var.gh_access_token
       repo_owner   = gh_content.repo_owner
       repo_name    = gh_content.repo_name
     })
-  } ] }
+  }] }
 }
