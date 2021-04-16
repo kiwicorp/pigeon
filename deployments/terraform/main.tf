8411c9643b5051e6591eca3fc616ed104ef107c2
cezarmathe <me@cezarmathe.com>
2021-04-16 21:31:45 +0300

    Improve demo deployment
    Improved demo deployment - now deploys an event for each repository.

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -25,40 +25,32 @@ module "gh_content_fn" {
   local_existing_package = "../../cmd/lambda/lambda_dev_linux_amd64.zip"
 }
 
-module "gh_content_event" {
+module "gh_content_events" {
   source  = "terraform-aws-modules/eventbridge/aws"
   version = "1.1.0"
 
   create_bus = false
 
-  rules = { for gh_content in var.gh_content : "event_${gh_content.repo_owner}_${gh_content.repo_name}" => {
+  rules = local.event_rules
+
+  targets = local.event_targets
+}
+
+locals {
+  event_rule_names = [for gh_content in var.gh_content : "event_${gh_content.repo_owner}_${gh_content.repo_name}"]
+
+  event_rules = { for index, gh_content in var.gh_content : local.event_rule_names[index] => {
     description         = "GitHub content - ${gh_content.repo_owner}/${gh_content.repo_name}."
     schedule_expression = "cron(0 0 ? * * *)"
   } }
 
-  # targets = { for gh_content in var.gh_content : "target_${gh_content.repo_owner}_${gh_content.repo_name}" => [
-  #   {
-  #     arn  = module.gh_content_fn.this_lambda_function_arn
-  #     name = "target_${gh_content.repo_owner}_${gh_content.repo_name}"
-  #     constant = jsonencode({
-  #         access_token = var.gh_access_token
-  #         repo_owner   = gh_content.repo_owner
-  #         repo_name    = gh_content.repo_name
-  #       })
-  #   }
-  # ] }
-
-  targets = {
-    schedule_lambda = [
-      {
-        arn  = module.demo_github_content_function.this_lambda_function_arn
-        name = "Execute lambda function"
-        constant = jsonencode({
-          access_token = "TODO!"
-          repo_owner   = "tokio-rs"
-          repo_name    = "tokio"
-        })
-      }
-    ]
-  }
+  event_targets = { for index, gh_content in var.gh_content : local.event_rule_names[index] => [ {
+    name     = "Execute lambda for ${gh_content.repo_owner}/${gh_content.repo_name}."
+    arn      = module.gh_content_fn.this_lambda_function_arn
+    constant = jsonencode({
+      access_token = var.gh_access_token
+      repo_owner   = gh_content.repo_owner
+      repo_name    = gh_content.repo_name
+    })
+  } ] }
 }
