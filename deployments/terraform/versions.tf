b0c5a1d4829b65f759921e58f1500c116693c300
cezarmathe <me@cezarmathe.com>
2021-04-26 12:37:25 +0300

    Adjust deployment versions required by terraform

--- deployments/terraform/versions.tf
+++ deployments/terraform/versions.tf
@@ -2,7 +2,11 @@
 
 terraform {
   required_providers {
-    aws = "~> 3.36"
+    aws        = "~> 3"
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2"
+    }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 0.15"
 }
