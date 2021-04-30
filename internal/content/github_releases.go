033e0b521c2f12ad621e8b76ac68b717fde55089
cezarmathe <me@cezarmathe.com>
2021-04-30 13:09:45 +0300

    Update github content handling
    Added better logic for sourcing the content, the creation of the mail
    body and the actual mailing.

--- internal/content/github_releases.go
+++ internal/content/github_releases.go
@@ -2,9 +2,12 @@ package content
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/selftechio/pigeon/internal/content/github_releases"
 	"github.com/selftechio/pigeon/internal/db"
+	"github.com/selftechio/pigeon/internal/mail"
 	"github.com/selftechio/pigeon/internal/model"
 )
 
@@ -25,30 +28,74 @@ func NewGithubReleasesContentHandler(accessToken, repoOwner, repoName, recipient
 }
 
 func (ch *GithubReleasesContentHandler) Handle(ctx context.Context) (bool, error) {
+	// get item from database
+	item := model.NewGithubRepositoryReleases(ch.RepoOwner, ch.RepoName)
+	err := db.GetItem(item)
+	if err != nil {
+		return false, err
+	}
+
+	// if the item has no total count, get the total count right now then return
+	// fixme 30/04/2021: this will produce no mail on first run.
+	//                   a better approach is to initialize the total count when the content urn is
+	//                   created in the db or sum stuff
+	if item.TotalCount == nil {
+		apiClient := github_releases.NewGithubV4ApiClient(ctx, ch.accessToken)
+		totalCount, err := apiClient.ReleasesTotalCount(ctx, ch.RepoOwner, ch.RepoName)
+		if err != nil {
+			return false, err
+		}
+		item.TotalCount = &totalCount
+		return false, nil
+	}
+
 	apiClient := github_releases.NewGithubV4ApiClient(ctx, ch.accessToken)
 
+	// check github releases count. if the same as before, return
+	// otherwise, update current total count
 	totalCount, err := apiClient.ReleasesTotalCount(ctx, ch.RepoOwner, ch.RepoName)
 	if err != nil {
 		return false, err
 	}
-
-	item := model.NewGithubRepositoryReleases(ch.RepoOwner, ch.RepoName)
-	err = db.GetItem(item)
-	if err != nil {
-		return false, err
-	}
-	if item.TotalCount == totalCount {
+	if totalCount == *item.TotalCount {
 		return false, nil
 	}
+	item.TotalCount = &totalCount
 
-	// fixme 24/04/2021: some situations may lead to a request that asks for more than 100 items,
-	// and that's prohibited by the github v4 api
-	releases, err := apiClient.LatestReleases(ctx, ch.RepoOwner, ch.RepoName, totalCount-item.TotalCount)
+	// get the diff of releases and cap it at 100, as allowed by the github api
+	// fixme 30/04/2021: use multiple requests to grab all releases
+	diff := totalCount - *item.TotalCount
+	if diff > 100 {
+		diff = 100
+	}
+
+	// get the new releases
+	releases, err := apiClient.LatestReleases(ctx, ch.RepoOwner, ch.RepoName, diff)
 	if err != nil {
 		return true, err
 	}
 
-	_, err = db.PutItem(releases)
+	// create the mail body
+	templateData := github_releases.GithubReleasesTemplateData{
+		Owner:    ch.RepoOwner,
+		Name:     ch.RepoName,
+		Releases: releases,
+	}
+	body, err := github_releases.CreateMailBody(&templateData)
+	if err != nil {
+		return true, err
+	}
+
+	// mail the new releases
+	mailer := mail.NewMailer()
+	err = mailer.SendMail([]string{ch.recipient}, fmt.Sprintf("New releases - %s/%s", ch.RepoOwner, ch.RepoName), body)
+	if err != nil {
+		log.Printf("email error: %#v", err)
+		return false, err
+	}
+
+	// update the total count
+	_, err = db.PutItem(item)
 	if err != nil {
 		return true, err
 	}
